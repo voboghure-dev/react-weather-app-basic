077c9aee3d999e753dcc87f7d9c0a9e1b81879ef
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-30 14:29:26 +0600

    Complete HTML part
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import axios from "axios";
+
 function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=f6875be64fc833e4c2f7500665728cdd`;
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dhaka</p>
+          </div>
+          <div className="temp">
+            <h1>60 deg</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65 deg</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
